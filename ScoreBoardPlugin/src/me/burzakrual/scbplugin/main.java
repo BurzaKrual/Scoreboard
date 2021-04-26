d40c37acb3f16a0cb5cdf1ebc7c7b8534f03e164
BurzaKrual <82435707+BurzaKrual@users.noreply.github.com>
2021-04-26 23:16:45 +0300

    Update main.java

--- ScoreBoardPlugin/src/me/burzakrual/scbplugin/main.java
+++ ScoreBoardPlugin/src/me/burzakrual/scbplugin/main.java
@@ -1,7 +1,45 @@
 package me.burzakrual.scbplugin;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class main extends  JavaPlugin{
-
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+ 
+public class main implements Listener {
+ 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        createScoreboard(e.getPlayer());
+        updateScoreboard();     
+    }
+    
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e){
+        updateScoreboard();
+    }
+    
+    public void createScoreboard(Player player){
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Objective objective = board.registerNewObjective("Stats", "dummy");
+        objective.setDisplayName("It looks awful for now :D");
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        Score score = objective.getScore("players:");
+        score.setScore(Bukkit.getOnlinePlayers().size());
+        player.setScoreboard(board);
+    }
+    
+    public void updateScoreboard(){
+        for(Player online : Bukkit.getOnlinePlayers()){
+            Score score = online.getScoreboard().getObjective(DisplaySlot.SIDEBAR).getScore("players:");
+            score.setScore(Bukkit.getOnlinePlayers().size());
+        }
+    }
 }
